[feat] Allow bagel to output logits and logprobs for sde, fix collator padding for padded images (#109)
* Apply bagel gen image learn and inferencer checkout patch

Co-authored-by: KemingWu <wukemingcqu@gmail.com>

* checkout patch collator for padding and clone for view backward ops

Co-authored-by: KemingWu <wukemingcqu@gmail.com>

* Remove debug lines

* Fix collator possible error for multiple vae images

* Update inferencer for non grpo config

---------

Co-authored-by: KemingWu <wukemingcqu@gmail.com>

src/lmms_engine/datasets/collator/bagel_collator.py
@@ -40,7 +40,19 @@ class BagelCollator:
         sequence_length = inputs.pop("sequence_length")
         sample_lens = inputs.pop("sample_lens")
         for keys, values in inputs.items():
-            if isinstance(values[0], torch.Tensor):
+            if keys == "padded_images":
+                num_images = sum([item.shape[0] for item in values])
+                image_width = [item.shape[2] for item in values]
+                image_height = [item.shape[3] for item in values]
+                max_image_width = max(image_width)
+                max_image_height = max(image_height)
+                padded_images = torch.zeros((num_images, 3, max_image_width, max_image_height), dtype=values[0].dtype)
+                index = 0
+                for image in values:
+                    padded_images[index : index + image.shape[0], :, : image.shape[2], : image.shape[3]] = image
+                    index += image.shape[0]
+                batched_inputs[keys] = padded_images
+            elif isinstance(values[0], torch.Tensor):
                 batched_inputs[keys] = torch.concatenate(values, dim=0)
             elif isinstance(values[0], list):
                 for value in values: